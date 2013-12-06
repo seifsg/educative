eda77e1d8db577bbb700536cd54e6085bcf191d9
Seif Sgayer <seifsg@users.noreply.github.com>
2013-12-06 10:32:27 -0800

    largest prime factor
    The prime factors of 13195 are 5, 7, 13 and 29.
    
    What is the largest prime factor of the number 600851475143 ?

--- largestprimefactor.cpp
+++ largestprimefactor.cpp
@@ -1,4 +1,3 @@
-
 //The prime factors of 13195 are 5, 7, 13 and 29.
 
 //What is the largest prime factor of the number 600851475143 ?
@@ -25,7 +24,7 @@ while(n>1){
 cout<<h;
 
 }
-
+    
 
 int main(){
 
